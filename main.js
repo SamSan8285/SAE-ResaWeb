e71bec8049538e22220ed0c787a00f3b9fca75dd
PITIPOU <146129977+SamSan8285@users.noreply.github.com>
2024-03-20 14:42:04 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
+
+  
